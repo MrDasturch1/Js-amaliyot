865a2d1e7124b58e39028ad832038bb7d83a506f
MrDasturch1 <xurshidjontemirov299@gmail.com>
2024-08-09 13:00:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let numberOfSeries = prompt('Nechta serial ko`rdingiz?', '')
+
+const seriesST = {
+    count: numberOfSeries,
+    series:{},
+    actors:{},
+    genres:[],
+    private:false
+}
+
+let savol1 = prompt("Oxirgi ko`rgan serialingiz?", '')
+let savol2 = prompt("Nechi baxo berasiz?", '')
+let savol3 = prompt("Oxirgi ko`rgan serialingiz?", '')
+let savol4 = prompt("Nechi baxo berasiz?", '')
+
+seriesST.series[savol1] = savol2;
+seriesST.series[savol3] = savol4;
+
+console.log(seriesST); 
