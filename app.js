efdd94b17ba4a85a2f19367603b909f636775f4c
MrDasturch1 <xurshidjontemirov299@gmail.com>
2024-08-10 07:41:22 +0300

    Update app.js
    First commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let numberOfSeries = prompt('Nechta serial ko`rdingiz?', '')
+let numberOfSeries = +prompt('Nechta serial ko`rdingiz?', '')
 
 const seriesST = {
     count: numberOfSeries,
@@ -11,9 +11,9 @@ const seriesST = {
 }
 
 let savol1 = prompt("Oxirgi ko`rgan serialingiz?", '')
-let savol2 = prompt("Nechi baxo berasiz?", '')
+let savol2 = +prompt("Nechi baxo berasiz?", '')
 let savol3 = prompt("Oxirgi ko`rgan serialingiz?", '')
-let savol4 = prompt("Nechi baxo berasiz?", '')
+let savol4 = +prompt("Nechi baxo berasiz?", '')
 
 seriesST.series[savol1] = savol2;
 seriesST.series[savol3] = savol4;
